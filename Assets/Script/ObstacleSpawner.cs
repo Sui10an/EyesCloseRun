e8415f1482bddd7b43450c824d453411b63306b8
Sui10an <go314513an@gmail.com>
2026-06-01 15:14:51 +0900

    Second

--- Assets/Script/ObstacleSpawner.cs
+++ Assets/Script/ObstacleSpawner.cs
@@ -2,16 +2,10 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [Header("発射する球")]
     public GameObject bulletPrefab;
 
-    [Header("プレイヤー")]
-    public Transform player;
-
-    [Header("発射速度")]
     public float bulletSpeed = 10f;
 
-    [Header("発射間隔")]
     public float fireInterval = 2f;
 
     private float timer;
@@ -29,22 +23,16 @@ public class ObstacleSpawner : MonoBehaviour
 
     void Shoot()
     {
-        // 球を生成
         GameObject bullet = Instantiate(
             bulletPrefab,
             transform.position,
             Quaternion.identity
         );
 
-        // プレイヤー方向を計算
-        Vector3 direction =
-            (player.position - transform.position).normalized;
-
-        // Rigidbody取得
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
 
-        // 発射
-        rb.linearVelocity = direction * bulletSpeed;
+        // X軸プラス方向へ飛ばす
+        rb.linearVelocity = transform.right * bulletSpeed;
     }
 
 }
