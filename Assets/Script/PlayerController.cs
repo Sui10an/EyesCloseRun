588e4f696cb4e15740cf63560e0a11141217d593
Sui10an <go314513an@gmail.com>
2026-06-19 15:56:54 +0900

    add

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
 
     private void Update()
     {
-        if (BlinkDetector.isclose)
+        if ((BlinkDetector.isclose || Input.GetKey(KeyCode.W)) && GameManager.isGameActive == true)
         {
+            animator.SetBool("isWalking", true);
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
             Debug.Log("Go!!");
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
         }
     }
 }
