c0aa2f775378efdddb89294d66111bfff2e5d0db
Sui10an <go314513an@gmail.com>
2026-06-22 17:31:03 +0900

    add.movefloor
    動く床が実装されました

---
+++ Assets/Script/MovingPlatform.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class MovingPlatform : MonoBehaviour
+{
+    public float moveDistance = 5f;
+    public float speed = 2f;
+    private Vector3 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    void Update()
+    {
+        float offset = Mathf.PingPong(Time.time * speed, moveDistance);
+        transform.position = startPos + new Vector3(0, 0, offset);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.root.SetParent(transform);
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.root.SetParent(null);
+        }
+    }
+}
