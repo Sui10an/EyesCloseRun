4569184d8c4016558555108b3bce0099663f1eaa
mimikkyu3 <mimikkyu3tora@gmail.com>
2026-06-25 16:11:37 +0900

    save
    コンベアのテクスチャが動くようになった

--- Assets/Script/ConveyorPlatform.cs
+++ Assets/Script/ConveyorPlatform.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class ConveyorPlatform : MonoBehaviour
 {
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float textureScrollScale = 0.1f;
+    [SerializeField] private Renderer targetRenderer;
 
     private Transform playerTransform;
+    private Vector2 textureOffset = Vector2.zero;
 
     void Update(){
         if(playerTransform != null){
             playerTransform.position +=Vector3.forward * speed * Time.deltaTime;
+        }
+        if(targetRenderer != null){
+            textureOffset.y +=speed * textureScrollScale * Time.deltaTime;
+            targetRenderer.material.mainTextureOffset = textureOffset;
         }
     }
 
