a0c7f1cf94d1a897cb60ca209737c0693c68359c
Sui10an <go314513an@gmail.com>
2026-07-10 19:29:46 +0900

    Stage_kari

--- Assets/Script/ConveyorPlatform.cs
+++ Assets/Script/ConveyorPlatform.cs
@@ -5,28 +5,43 @@ public class ConveyorPlatform : MonoBehaviour
     [SerializeField] private float speed = 2f;
     [SerializeField] private float textureScrollScale = 0.1f;
     [SerializeField] private Renderer targetRenderer;
+    [SerializeField] private bool upRotation = true;
 
     private Transform playerTransform;
     private Vector2 textureOffset = Vector2.zero;
 
-    void Update(){
-        if(playerTransform != null){
-            playerTransform.position +=Vector3.forward * speed * Time.deltaTime;
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            playerTransform.position += Vector3.forward * speed * Time.deltaTime;
         }
-        if(targetRenderer != null){
-            textureOffset.y +=speed * textureScrollScale * Time.deltaTime;
+        if (targetRenderer != null)
+        {
+            if (upRotation)
+            {
+                textureOffset.y += speed * textureScrollScale * Time.deltaTime;
+            }
+            else
+            {
+                textureOffset.y -= speed * textureScrollScale * Time.deltaTime;
+            }
             targetRenderer.material.mainTextureOffset = textureOffset;
         }
     }
 
-    void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.CompareTag("Player")){
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
             playerTransform = collision.transform.root;
         }
     }
 
-    void OnCollisionExit(Collision collision){
-        if(collision.gameObject.CompareTag("Player")){
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
             playerTransform = null;
         }
     }
