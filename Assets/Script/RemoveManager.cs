e61d13df9adae6f1ee701211457dfb15ac09f210
Sui10an <go314513an@gmail.com>
2026-07-11 21:55:49 +0900

    respawn
    リスポーンを治した

--- Assets/Script/RemoveManager.cs
+++ Assets/Script/RemoveManager.cs
@@ -9,8 +9,8 @@ public class RemoveManager : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Transform player = other.transform.parent;
-            player.position = new Vector3(player.position.x, 0, player.position.z - 20f);
-            other.transform.position = new Vector3(RespaewnPoint.position.x, RespaewnPoint.position.y, RespaewnPoint.position.z);
+            player.position = new Vector3(player.position.x, 0, RespaewnPoint.position.z);
+            other.transform.position = new Vector3(RespaewnPoint.position.x, RespaewnPoint.position.y, other.transform.position.z);
             GameManager.Instance.TakeDamage(); // ← ダメージを通知
         }
     }
